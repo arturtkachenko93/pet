c7ecbd157239528bda5fa8b1f80e2a5faa6ec71e
Artur Tkachenko <atkachenko2@alfabank.ru>
2024-09-06 17:27:20 +0300

    Прошелся stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
     rules: {
         'import-notation': 'string',
+        'selector-class-pattern': '^([a-z0-9]+(?:-[a-z0-9]+)*)|([a-z0-9]+(?:[A-Z][a-z0-9]+)*)$',
     },
 };
